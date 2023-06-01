0bf0dbe12ffa3f0e9eac6c5e50941f537c50092a
youngbinary <youngbin@barogo.com>
2023-06-01 14:10:54 +0900

    Changes

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class DemoApplication
 
-// TODO: help
+// TODO: help22
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
 }
